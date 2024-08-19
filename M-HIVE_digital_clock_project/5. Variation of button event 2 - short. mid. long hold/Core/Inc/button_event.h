ea5f0a75cdaac2b5b382e2a7b60582d9e52747f7
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-08-19 21:29:24 +0900

    code update.

---
+++ M-HIVE_digital_clock_project/5. Variation of button event 2 - short. mid. long hold/Core/Inc/button_event.h
@@ -0,0 +1,14 @@
+/* functions for handle button event. */
+void button1_start();
+void button1_show_press_time();
+
+void button1_measure_hold();
+void button1_increase_value_by_hold_state();
+void button1_short_hold();
+void button1_mid_hold();
+void button1_long_hold();
+
+void button1_measure_release();
+void button1_short_release();
+void button1_mid_release();
+void button1_long_release();
